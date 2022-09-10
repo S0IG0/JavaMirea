588f3936099214923c7468d62c158923b0cb6f44
Даниил <dchibitok@yandex.ru>
2022-09-11 01:12:42 +0300

    9 практическая работа

---
+++ src/main/java/com/example/java__/Shape.java
@@ -0,0 +1,58 @@
+package com.example.java__;
+
+import javafx.scene.paint.Color;
+
+public abstract class Shape {
+    String color;
+    int x;
+    int y;
+    Color color_;
+
+    public Shape(String color, int x, int y, Color color_) {
+        this.color = color;
+        this.color_ = color_;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Color getColor_() {
+        return color_;
+    }
+
+    public void setColor_(Color color_) {
+        this.color_ = color_;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
