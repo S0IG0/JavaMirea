b3e955c7a45e9099a02226aa7b025cba60e71d15
Даниил <dchibitok@yandex.ru>
2022-09-14 15:15:57 +0300

    12 практическая работа

--- src/main/java/Practic_12/Main4.java
+++ src/main/java/Practic_12/Main4.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main4 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample4.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main4.class.getResource("sample4.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
